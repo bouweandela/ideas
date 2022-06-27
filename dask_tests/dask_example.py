c21546af65f97fac7bd1c76a2f0da349689bb65d
Bouwe Andela <b.andela@esciencecenter.nl>
2022-06-27 16:42:00 +0200

    Add another dask example

---
+++ dask_tests/dask_example.py
@@ -0,0 +1,34 @@
+import sys
+from pathlib import Path
+
+import dask.distributed
+import dask
+import iris
+import xarray
+
+def compute_example(in_file, out_file):
+    cube = iris.load_cube(in_file)
+    cube = cube.collapsed('time', iris.analysis.MEAN)
+    #result = iris.save(cube, out_file, compute=False)
+    ds = xarray.DataArray.from_iris(cube)
+    result = ds.to_netcdf(out_file, compute=False)
+    return result
+
+def main(in_files):
+    # start a dask cluster, e.g.
+    cluster = dask.distributed.LocalCluster()
+    # or when on Jasmin/Levante/etc:
+    # cluster = dask_jobqueue.SlurmCluster()
+    # or a even cluster in the cloud hopefully at some point
+    client = dask.distributed.Client(cluster)
+
+    results = []
+    for i, in_file in enumerate(in_files):
+        out_file = Path(f'result{i}.nc')
+        result = compute_example(in_file, out_file)
+        results.append(result)
+
+    dask.compute(*results)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
